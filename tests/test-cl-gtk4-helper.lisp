3009e38f3f263b1ed484286f1f32543c0d320099
Jonathan A. Bennett <doulos05@gmail.com>
2023-09-30 11:06:05 +0900

    Initial Tests.

--- tests/test-cl-gtk4-helper.lisp
+++ tests/test-cl-gtk4-helper.lisp
@@ -3,10 +3,38 @@
 ;; Define your project tests here...
 
 (def-suite testmain
-    :description "test suite 1")
+    :description "Primary Test Suite")
 
 (in-suite testmain)
 
-(test test1
+(test create-empty-string-list
+  (is (= (+ 1 1)
+         3)))
+
+(test create-string-list-from-source
+  (is (= (+ 1 1)
+         3)))
+
+(test create-string-list-with-filter
+  (is (= (+ 1 1)
+         3)))
+
+(test add-label-column
+  (is (= (+ 1 1)
+         3)))
+
+(test add-color-column
+  (is (= (+ 1 1)
+         3)))
+
+(test adding-items
+  (is (= (+ 1 1)
+         3)))
+
+(test update-list
+  (is (= (+ 1 1)
+         3)))
+
+(test search-for-column
   (is (= (+ 1 1)
          3)))
